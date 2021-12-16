700bb2934bf4bcc2b786a1e1669f9b7af7bfbb9d
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-16 12:17:34 -0300

    test: improvement test cases

--- src/data/usecases/local-launch-coin-randomly.spec.ts
+++ src/data/usecases/local-launch-coin-randomly.spec.ts
@@ -2,10 +2,34 @@ import { Result } from '@/domain/models'
 import { LocalLaunchCoinRandomly } from './local-launch-coin-randomly'
 
 describe('LocalLaunchCoinRandomly', () => {
-  test('Should be return heads or tails when call launch method', () => {
+  afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore()
+  })
+
+  test('Should return heads if random number less than 0.5', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.49)
+
     const sut = new LocalLaunchCoinRandomly()
     const result = sut.launch()
 
-    expect([Result.Heads, Result.Tails].includes(result)).toBeTruthy()
+    expect((result)).toBe(Result.Heads)
+  })
+
+  test('Should return tails if random number greater than 0.5', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.6)
+
+    const sut = new LocalLaunchCoinRandomly()
+    const result = sut.launch()
+
+    expect((result)).toBe(Result.Tails)
+  })
+
+  test('Should return tails if random number equal than 0.5', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.5)
+
+    const sut = new LocalLaunchCoinRandomly()
+    const result = sut.launch()
+
+    expect((result)).toBe(Result.Tails)
   })
 })
