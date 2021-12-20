2da5b3c2b807f1da9695e234b6f7ee73668cda46
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 16:38:54 -0300

    refactor: move creation SUT to a factory method

--- src/data/usecases/local-flip-coin-randomly.spec.ts
+++ src/data/usecases/local-flip-coin-randomly.spec.ts
@@ -1,33 +1,38 @@
 import { Result } from '@/domain/models'
 import { LocalFlipCoinRandomly } from './local-flip-coin-randomly'
 
+type SutTypes = {
+  sut: LocalFlipCoinRandomly
+}
+
+const makeSut = (value: number): SutTypes => {
+  jest.spyOn(global.Math, 'random').mockReturnValue(value)
+  return {
+    sut: new LocalFlipCoinRandomly()
+  }
+}
+
 describe('LocalFlipCoinRandomly', () => {
   afterEach(() => {
     jest.spyOn(global.Math, 'random').mockRestore()
   })
 
   test('Should return heads if random number less than 0.5', () => {
-    jest.spyOn(global.Math, 'random').mockReturnValue(0.49)
-
-    const sut = new LocalFlipCoinRandomly()
+    const { sut } = makeSut(0.49)
     const result = sut.flip()
 
     expect((result)).toBe(Result.Heads)
   })
 
   test('Should return tails if random number greater than 0.5', () => {
-    jest.spyOn(global.Math, 'random').mockReturnValue(0.6)
-
-    const sut = new LocalFlipCoinRandomly()
+    const { sut } = makeSut(0.6)
     const result = sut.flip()
 
     expect((result)).toBe(Result.Tails)
   })
 
   test('Should return tails if random number equal than 0.5', () => {
-    jest.spyOn(global.Math, 'random').mockReturnValue(0.5)
-
-    const sut = new LocalFlipCoinRandomly()
+    const { sut } = makeSut(0.5)
     const result = sut.flip()
 
     expect((result)).toBe(Result.Tails)
