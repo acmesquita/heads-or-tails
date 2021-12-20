bfa71cae76fec77e215c4903c50ddb22877ed785
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:24:22 -0300

    refactor: move creation SUT to a factory method

--- src/data/usecases/save-players-in-context.spec.ts
+++ src/data/usecases/save-players-in-context.spec.ts
@@ -3,15 +3,29 @@ import { InvalidParamsError } from '@/domain/errors'
 import { ContextSpy } from '@/data/test'
 import { SavePlayersInContext } from './save-players-in-context'
 
+type SutTypes = {
+  sut: SavePlayersInContext
+  contextSpy: ContextSpy
+}
+
+const makeSut = (): SutTypes => {
+  const contextSpy = new ContextSpy()
+  const sut = new SavePlayersInContext(contextSpy)
+
+  return {
+    sut,
+    contextSpy
+  }
+}
+
 describe('SavePlayersInContext', () => {
   test('Should throws InvalidParamsError if invalid params are provider', () => {
-    const sut = new SavePlayersInContext(null)
+    const { sut } = makeSut()
     expect(sut.save).toThrowError(new InvalidParamsError())
   })
 
   test('Should save correctly values', () => {
-    const contextSpy = new ContextSpy()
-    const sut = new SavePlayersInContext(contextSpy)
+    const { sut, contextSpy } = makeSut()
     const player1: Player = {
       name: 'any_name',
       coinFace: Result.Heads
