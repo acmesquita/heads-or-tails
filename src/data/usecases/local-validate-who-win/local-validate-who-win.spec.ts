46435f819e76d4a3f374a947887eec86d2b4d5a5
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:49:43 -0300

    test: ensure LocalValidateWhoWin throws if player2 not is provider

--- src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
+++ src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
@@ -1,10 +1,21 @@
 import { InvalidParamsError } from '@/domain/errors'
+import { Player, Result } from '@/domain/models'
 import { LocalValidateWhoWin } from './local-validate-who-win'
 
 describe('LocalValidateWhoWin', () => {
-  test('Should throws if player1 not is provider', () => {
+  test('Should throws InvalidParamsError if player1 not is provider', () => {
     const sut = new LocalValidateWhoWin()
 
-    expect(() => sut.validate(null)).toThrowError(new InvalidParamsError())
+    expect(() => sut.validate(null, null)).toThrowError(new InvalidParamsError())
+  })
+
+  test('Should throws InvalidParamsError if player2 not is provider', () => {
+    const sut = new LocalValidateWhoWin()
+    const player1: Player = {
+      name: 'any_name',
+      coinFace: Result.Heads
+    }
+
+    expect(() => sut.validate(player1, null)).toThrowError(new InvalidParamsError())
   })
 })
