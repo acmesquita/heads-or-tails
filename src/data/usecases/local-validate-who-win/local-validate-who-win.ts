dd0b40a9b25c7abff663124be5ba53191c89fa27
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:59:47 -0300

    feat: ensure LocalValidateWhoWin validate who player choice coin face equal to faceResult

--- src/data/usecases/local-validate-who-win/local-validate-who-win.ts
+++ src/data/usecases/local-validate-who-win/local-validate-who-win.ts
@@ -1,10 +1,18 @@
 import { InvalidParamsError } from '@/domain/errors'
 import { Player, Result } from '@/domain/models'
+import { ValidateWhoWin } from '@/domain/usecases'
 
-export class LocalValidateWhoWin {
-  validate (player1: Player, player2: Player, faceResult: Result): void {
+export class LocalValidateWhoWin implements ValidateWhoWin {
+  validate (player1: Player, player2: Player, faceResult: Result): Player {
     if (!player1 || !player2 || !faceResult) {
       throw new InvalidParamsError()
+    }
+    if (player1.coinFace === faceResult) {
+      return player1
+    }
+
+    if (player2.coinFace === faceResult) {
+      return player2
     }
   }
 }
