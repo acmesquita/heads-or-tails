05e33e71f3ddf2cb0f44fd2029e0ddac871299ee
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:15:56 -0300

    feat: ensure SavePlayersInContext save correctly values

--- src/data/usecases/save-players-in-context.ts
+++ src/data/usecases/save-players-in-context.ts
@@ -1,10 +1,17 @@
 import { InvalidParamsError } from '@/domain/errors'
 import { SavePlayers } from '@/domain/usecases'
+import { SaveContext } from '@/data/protocols/save-context'
 
 export class SavePlayersInContext implements SavePlayers {
+  constructor (
+    private readonly context: SaveContext
+  ) {}
+
   save (params: SavePlayers.Param): void {
     if (!params) {
       throw new InvalidParamsError()
     }
+
+    this.context.save(params)
   }
 }
