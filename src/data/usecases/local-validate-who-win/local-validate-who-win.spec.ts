1e0d9b3bdbaf64fcad054e479ffcfa5c94a52c1d
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:51:02 -0300

    refactor: move creation SUT to a factory method

--- src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
+++ src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
@@ -2,15 +2,19 @@ import { InvalidParamsError } from '@/domain/errors'
 import { Player, Result } from '@/domain/models'
 import { LocalValidateWhoWin } from './local-validate-who-win'
 
+const makeSut = (): LocalValidateWhoWin => {
+  return new LocalValidateWhoWin()
+}
+
 describe('LocalValidateWhoWin', () => {
   test('Should throws InvalidParamsError if player1 not is provider', () => {
-    const sut = new LocalValidateWhoWin()
+    const sut = makeSut()
 
     expect(() => sut.validate(null, null)).toThrowError(new InvalidParamsError())
   })
 
   test('Should throws InvalidParamsError if player2 not is provider', () => {
-    const sut = new LocalValidateWhoWin()
+    const sut = makeSut()
     const player1: Player = {
       name: 'any_name',
       coinFace: Result.Heads
