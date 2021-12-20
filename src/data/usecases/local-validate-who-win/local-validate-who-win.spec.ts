74c3d445e4bdeafe20baee4c76f381207e3d9ff2
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:53:15 -0300

    test: ensure LocalValidateWhoWin throws if faceResult not is provider

--- src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
+++ src/data/usecases/local-validate-who-win/local-validate-who-win.spec.ts
@@ -10,7 +10,7 @@ describe('LocalValidateWhoWin', () => {
   test('Should throws InvalidParamsError if player1 not is provider', () => {
     const sut = makeSut()
 
-    expect(() => sut.validate(null, null)).toThrowError(new InvalidParamsError())
+    expect(() => sut.validate(null, null, null)).toThrowError(new InvalidParamsError())
   })
 
   test('Should throws InvalidParamsError if player2 not is provider', () => {
@@ -20,6 +20,20 @@ describe('LocalValidateWhoWin', () => {
       coinFace: Result.Heads
     }
 
-    expect(() => sut.validate(player1, null)).toThrowError(new InvalidParamsError())
+    expect(() => sut.validate(player1, null, null)).toThrowError(new InvalidParamsError())
+  })
+
+  test('Should throws InvalidParamsError if faceResult not is provider', () => {
+    const sut = makeSut()
+    const player1: Player = {
+      name: 'any_name',
+      coinFace: Result.Heads
+    }
+    const player2: Player = {
+      name: 'any_name',
+      coinFace: Result.Tails
+    }
+
+    expect(() => sut.validate(player1, player2, null)).toThrowError(new InvalidParamsError())
   })
 })
