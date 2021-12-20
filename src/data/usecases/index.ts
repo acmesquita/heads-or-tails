8d7516f15c9998efa681ed318bb1bc9782e9d4ae
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:30:26 -0300

    refactor: move files to folders

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,2 +1,2 @@
-export * from './local-flip-coin-randomly'
-export * from './save-players-in-context'
+export * from './local-flip-coin-randomly/local-flip-coin-randomly'
+export * from './save-players-in-context/save-players-in-context'
