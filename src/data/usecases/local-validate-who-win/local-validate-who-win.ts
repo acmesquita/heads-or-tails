57dca6892a83472aea03378ecbc9069a3c5cb755
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-12-20 17:49:28 -0300

    feat: ensure LocalValidateWhoWin throws if player2 not is provider

--- src/data/usecases/local-validate-who-win/local-validate-who-win.ts
+++ src/data/usecases/local-validate-who-win/local-validate-who-win.ts
@@ -2,8 +2,8 @@ import { InvalidParamsError } from '@/domain/errors'
 import { Player } from '@/domain/models'
 
 export class LocalValidateWhoWin {
-  validate (player1: Player): void {
-    if (!player1) {
+  validate (player1: Player, player2: Player): void {
+    if (!player1 || !player2) {
       throw new InvalidParamsError()
     }
   }
